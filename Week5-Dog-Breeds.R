e2e85c1debf8f47a32740cc80235938dec08be5c
muhammet-ozkaraca <79028024+muhammet-ozkaraca@users.noreply.github.com>
2022-02-19 23:50:14 +0300

    Week6-TuskegeeAirmen

---
+++ Week5-Dog-Breeds.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(ggplot2)
+library(showtext)
+library(gt)
+library(gtExtras)
+library(ggtext)
+library(tidytuesdayR)
+
+
+breed_traits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_traits.csv')
+trait_description <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/trait_description.csv')
+breed_rank_all <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_rank.csv')
+
+colnames(breed_rank_all)
+colnames(rank)
+
+breed_rank_all$`2020 Rank`
+
+rank <- breed_rank_all %>%
+  select(Breed, `2020 Rank`, Image) %>%
+  arrange(`2020 Rank`) %>%
+  slice (1:3) 
+
+gt::gt(rank) %>%
+  gt_theme_nytimes () %>%
+  tab_header(title = html("<strong>2020 Top Breeds</strong>")) %>%
+  gtExtras::gt_img_rows(columns = Image, height = 65)
+
+# breed_data1 <- merge(breed_traits, breed_rank_all, by = "Breed")
+# breed_data2 <- left_join(breed_traits, breed_rank_all, by = "Breed")
+# breed_data3 <- inner_join(breed_traits, breed_rank_all, by = "Breed")
+# breed_data4 <- full_join(breed_traits, breed_rank_all, by = "Breed")
+
+breed_data <- right_join(breed_traits, breed_rank_all, by = "Breed")
+
+
+
+
