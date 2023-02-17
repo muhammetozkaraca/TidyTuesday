9dea60d650ff19ea519c0cef33987885cea9c7d9
muhammetozkaraca <muhammetozk@icloud.com>
2023-02-17 15:11:16 +0300

    Week7-Updated

--- 2023/Week7-HollywoodAge/hollywood-age.R
+++ 2023/Week7-HollywoodAge/hollywood-age.R
@@ -21,9 +21,7 @@ font_add('fa-solid', 'fonts/Font Awesome 6 Free-Solid-900.otf')
 
 showtext_auto()
 
-age_gaps <- age_gaps %>%
-  clean_names(case = "lower_camel", abbreviations = c("ID", "DOB")) # abbreviations argument does tell {clean_names} to leave certain abbreviations untouched:
-
+# Film Directors
 
 age_gaps <- age_gaps %>%
   pivot_longer(actor_1_name:actor_2_age,
@@ -70,27 +68,41 @@ age_gaps <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/t
 
 age_gaps<- age_gaps %>%
   mutate(period = case_when(release_year > 1999 ~ 1,
-                            release_year < 2000 ~ 0))
+                            release_year < 2000 ~ 0)) %>%
+  mutate(period = as.factor(period))
 
 
 ggplot(age_gaps, aes(x = age_man, y = age_woman, color = period, alpha = 0.5)) +
   geom_jitter() +
   scale_x_continuous(limits = c(min(age_gaps$age_man), max(age_gaps$age_man))) +
   scale_y_continuous(limits = c(min(age_gaps$age_woman), max(age_gaps$age_woman))) +
-  geom_smooth(linetype = "dashed", color = "lightblue", alpha = 0.7) +
+  geom_smooth(linetype = "dashed", color = "#023047", alpha = 0.7, se = FALSE) +
+  scale_color_manual(values = c("#219ebc", "#fb8500"),
+                     labels = c("Pre 2000s", "Post 2000s")) +
+  guides(alpha = "none") + #to remove alpha legend from the visualization
   labs(title = "Male and Female Actors' Ages in Hollywood Movies",
        subtitle = "Data includes <span style='font-weight:bold; color:#ff9f1c;'>1155 Hollywood movies</span> that were released between <span style='color:#ff9f1c;'>1935</span> and <span style='color:#ff9f1c;'>2022</span>.",
        x = "Male Actor Age",
        y = "Female Actor Age",
+       color = "Period", 
        caption = "Source: Hollywood Age Gap | Plot: <span style='font-family:fa-brands'>&#xf09b;</span> muhammetozkaraca <span style='font-family:fa-brands'>&#xf099;</span> muhammetozkrca | #TidyTuesday-Week 7") +
-  theme(legend.position = "none",
-        axis.title.y = element_markdown(family = "axis", size = 55, linewidth = 0.2),
+  theme_minimal() +
+  theme(axis.title.y = element_markdown(family = "axis", size = 55, linewidth = 0.2),
         axis.title.x = element_markdown(family = "axis", size = 55, linewidth = 0.2),
         axis.text.y = element_markdown(family = "axis", size = 45),
         axis.text.x = element_markdown(family = "axis", hjust = 0.43, size = 45),
         plot.title = element_markdown(family = "title", size = 95, hjust = 0.5, lineheight = 0.15, linewidth = 0.1),
         plot.subtitle = element_markdown(family = "subtitle", size = 75, hjust = 0.5, lineheight = 0.15, linewidth = 0.1),
-        plot.caption = element_markdown(family = "title", size = 45, lineheight = 0.15, linewidth = 0.1, hjust = 0.5))
+        plot.caption = element_markdown(family = "title", size = 45, lineheight = 0.15, linewidth = 0.1, hjust = 0.5),
+        plot.background = element_rect(fill = "white"),
+        # legend.position = c(0.05, 0.9), 
+        legend.position = "top",
+        legend.justification = c(0, 0),
+        legend.spacing.x = unit(0.1, 'cm'), 
+        legend.direction = "horizontal",
+        legend.title = element_markdown(face="bold", family = "title", size = 55),
+        legend.text = element_markdown(face="bold", family = "title", size = 45),
+        legend.key = element_blank())
 
 ggsave("/Users/muhammetozkaraca/Desktop/male-female-ages.png", height = 6,  width = 7.5, dpi = 720)
 
