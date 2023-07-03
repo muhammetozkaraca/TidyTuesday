8fc741ad411ae85a3cdac89bd0f8cd69017dc1e7
muhammetozkaraca <muhammetozk@icloud.com>
2023-07-03 18:09:21 +0300

    figuring-out-an-issue

--- 2023/Week_26_USPopulatedPlaces/code.R
+++ 2023/Week_26_USPopulatedPlaces/code.R
@@ -21,32 +21,54 @@ us_place_history <- readr::read_csv('https://raw.githubusercontent.com/rfordatas
 
 us_place_names_tidied <- us_place_names %>%
   drop_na(prim_long_dec, prim_lat_dec) %>%
+  # filter(prim_lat_dec < 50, prim_lat_dec > 15, prim_long_dec < -68, prim_long_dec > -140) %>% 
+  # st_as_sf(coords = c("prim_long_dec", "prim_lat_dec"), crs=4326) %>%
   st_as_sf(coords = c("prim_long_dec", "prim_lat_dec"), crs=4326) %>%
-  st_crop(xmin = -140, xmax = -55, ymin = 15, ymax = 50) %>%
+  st_crop(xmin = -140, xmax = -68, ymin = 15, ymax = 50) %>%
   st_transform(crs = st_crs("+proj=laea +lat_0=45 +lon_0=-100")) 
+
+lon <- c(-140, -64)
+lat <- c(10, 51)
+
+poly = data.frame(lon, lat) |>
+  sf::st_as_sf(coords = c("lon", "lat"), crs = "EPSG:4326") |> 
+  sf::st_bbox() |> 
+  sf::st_as_sfc() |>
+  sf::st_as_sf()
+
+polc <- sf::st_make_grid(poly, n = c(20, 20), crs = "EPSG:4326") |>
+  sf::st_as_sf()
+
 
 usa <- maps::map("world", fill=TRUE, plot =FALSE) %>% # a different way to load world countries' geo data into the working environment
   st_as_sf() %>% 
-  filter(ID %in% c("USA")) %>%
-  st_crop(xmin = -140, xmax = -55, ymin = 25, ymax = 50) %>%
-  st_transform(crs = st_crs("+proj=laea +lat_0=45 +lon_0=-100")) 
+  filter(ID %in% c("USA", "Canada")) %>%
+  sf::st_transform(crs = "EPSG:4326") |>
+  sf::st_crop(xmin = -140, xmax = -64, ymin = 10, ymax = 51)
+
+usa_cropped <-  sf::st_intersection(usa, polc) |>
+  dplyr::group_by(ID) |>
+  dplyr::summarise() %>%
+  sf::st_transform(crs = sf::st_crs("+proj=laea +lat_0=45 +lon_0=-100"))
+
 
 
 lakes <- maps::map("lakes", fill=TRUE, plot =FALSE) %>%
   st_as_sf() %>%
-  st_crop(xmin = -140, xmax = -55, ymin = 15, ymax = 55) %>%
+  st_crop(xmin = -140, xmax = -55, ymin = 15, ymax = 40) %>%
   st_transform(crs = st_crs("+proj=laea +lat_0=45 +lon_0=-100")) 
 
 
 plot <- ggplot() +
-  geom_sf(data = usa, fill = "#1D7874", color = "lightblue") +
+  geom_sf(data = usa_cropped, fill = "#1D7874") +
+  geom_sf(data = usa %>% filter(ID == "USA"), fill = "#231709", color = "black") +
   geom_sf(data = lakes , fill = "lightblue", color = "transparent") +
-  geom_sf(data = us_place_names_tidied, size = 0.0000001, color = "#FF8551", alpha = 0.05) +
+  geom_sf(data = us_place_names_tidied, size = 0.0000001, color = "#FF8551", alpha = 0.1) +
   coord_sf(crs = st_crs("+proj=laea +lat_0=45 +lon_0=-100")) +
   scale_x_continuous(labels = function(x) paste0(x, "° W")) +
   scale_y_continuous(labels = function(y) paste0(y, "° N")) +
-  labs(title = "US Populated Places",
-       caption = "Source: GNIS | Plot: <span style='font-family:fa-brands'>&#xf09b;</span> muhammetozkaraca <span style='font-family:fa-brands'>&#xf099;</span> @muhammetozkrca | #TidyTuesday-Week 26") +
+  labs(title = "US Populated Places") +
+       # caption = "Source: GNIS | Plot: <span style='font-family:fa-brands'>&#xf09b;</span> muhammetozkaraca <span style='font-family:fa-brands'>&#xf099;</span> @muhammetozkrca | #TidyTuesday-Week 26") +
   theme(plot.background = element_rect(fill = "lightblue", color = "lightblue"),
         panel.background = element_rect(fill = "lightblue", color = "lightblue"),
         plot.title = element_markdown(size = 100, hjust = 0.5, family = "title", face = "bold", linewidth = 0.3),
